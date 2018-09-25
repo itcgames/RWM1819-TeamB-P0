bcbafa09270f0a945fd9ab7d6f13f99c9af5ebff
DJ O'Leary <djoleary39@ymail.com>
2018-09-25 14:41:17 +0100

    Added added test for modulo_first

--- test/p0TeamB.js
+++ test/p0TeamB.js
@@ -9,6 +9,18 @@ describe('p0TeamB()', function () {
   });
 
   it('Cubed of 2', function () {
-    expect(p0TeamB(2)).to.equal(8);
+    expect(Cube(2)).to.equal(8);
+  });
+
+  it('modulo_first of 2 into 2, 3, 6, and 5', function () {
+    expect(modulo_first([2, 3, 6, 5])).to.deep.equal([0,1,0,1]);
+  });
+
+  it("Modulo_3 of 3", function () {
+    expect(Modulo_3(3)).to.equal(0);
+  });
+
+  it("Modulo_3 of 7", function () {
+    expect(Modulo_3(7)).to.equal(1);
   });
 });
