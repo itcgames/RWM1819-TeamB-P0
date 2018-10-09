dd6beb952d3f9a6c26fec5f7cce568e98b4b325d
Peter Daly <peterd060397@gmail.com>
2018-10-09 11:06:59 +0100

    Added last 5 tests

--- test/p0TeamB.js
+++ test/p0TeamB.js
@@ -20,23 +20,32 @@ describe('p0TeamB()', function () {
     expect(Modulo_3(3)).to.equal(0);
   });
 
-  it("Modulo_3 of 7", function () {
-    expect(Modulo_3(7)).to.equal(1);
-  });
-
   it("running_total of 2, 3, 7", function () {
     expect(running_total([2, 3, 7])).to.equal(12);
   });
 
-  it("running_total of -2, 3, -7", function () {
-    expect(running_total([-2, 3, -7])).to.equal(-6);
-  });
-
-  it("decrement < first: 4, 3, 5", function(){
-    expect(decrement_if_less_than_first([4,3,5])).to.deep.equal([4,2,5]);
-  });
 
   it("decrement < first: 10, -1, null", function(){
     expect(decrement_if_less_than_first([10,-1,null])).to.deep.equal([10,-2,-1]);
+  });
+
+  it("xor_21 of 4", function() {
+    expect(xor_21(4)).to.equal(17);
+  });
+
+  it("collatz of 3", function() {
+    expect(collatz(3)).to.equal(10);
+  });
+
+  it("diff of 4, 2, 8, 3, 9, 4, 10", function() {
+    expect(diff([4, 2, 8, 3, 9, 4, 10])).to.deep.equal([4, 2, 6, 5, 6, 5, 6]);
+  });
+
+  it("fact of 8", function() {
+    expect(fact(8)).to.equal(40320);
+  });
+
+  it("fizzbuzz of 30", function() {
+    expect(fizzbuzz(30)).to.equal(0);
   });
 });
