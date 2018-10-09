933cac88fcb14054339f23c7d7553d9c8d6f2d9f
Peter Daly <peterd060397@gmail.com>
2018-10-09 11:59:56 +0100

    Set up fizzbuzz

--- src/p0TeamB.js
+++ src/p0TeamB.js
@@ -2,5 +2,35 @@
 'use strict';
 
 var p0TeamB = function (list) {
-  
+  list.forEach(element =>  {
+    element = Cube(element);
+  });
+
+  list.forEach(element =>  {
+    element = Modulo_3(element);
+  });
+
+  list = modulo_first(list);
+
+  list = decrement_if_less_than_first(list);
+
+  list = running_total(list);
+
+  list = diff(list);
+
+  list.forEach(element =>  {
+    element = fact(element);
+  });
+
+  list.forEach(element =>  {
+    element = xor_21(element);
+  });
+
+  list.forEach(element =>  {
+    element = collatz(element);
+  });
+
+  element = fizzbuzz(element);
+
+  return list;
 }
--- test/p0TeamB.js
+++ test/p0TeamB.js
@@ -3,49 +3,9 @@
 describe('p0TeamB()', function () {
   'use strict';
 
-  it('exists', function () {
-    expect(p0TeamB).to.be.a('function');
+  var list = [];
 
-  });
-
-  it('Cubed of 2', function () {
-    expect(Cube(2)).to.equal(8);
-  });
-
-  it('modulo_first of 2 into 2, 3, 6, and 5', function () {
-    expect(modulo_first([2, 3, 6, 5])).to.deep.equal([0,1,0,1]);
-  });
-
-  it("Modulo_3 of 3", function () {
-    expect(Modulo_3(3)).to.equal(0);
-  });
-
-  it("running_total of 2, 3, 7", function () {
-    expect(running_total([2, 3, 7])).to.equal(12);
-  });
-
-
-  it("decrement < first: 10, -1, null", function(){
-    expect(decrement_if_less_than_first([10,-1,null])).to.deep.equal([10,-2,-1]);
-  });
-
-  it("xor_21 of 4", function() {
-    expect(xor_21(4)).to.equal(17);
-  });
-
-  it("collatz of 3", function() {
-    expect(collatz(3)).to.equal(10);
-  });
-
-  it("diff of 4, 2, 8, 3, 9, 4, 10", function() {
-    expect(diff([4, 2, 8, 3, 9, 4, 10])).to.deep.equal([4, 2, 6, 5, 6, 5, 6]);
-  });
-
-  it("fact of 8", function() {
-    expect(fact(8)).to.equal(40320);
-  });
-
-  it("fizzbuzz of 30", function() {
-    expect(fizzbuzz(30)).to.equal(0);
+  it("Super filter of  ", function() {
+    expect(p0TeamB([2, 4, 6])).to.deep.equal([2, 4, 6]);
   });
 });
